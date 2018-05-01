284ad377c9b06fff1676bb7eab88984dacd5cd85
Bauke Conijn <bcmpinc@users.sourceforge.net>
2018-05-01 21:46:44 +0200

    Mod now compiles

---
+++ AlwaysScrollMap/ModEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace AlwaysScrollMap
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {}
+
+    }
+}
+
