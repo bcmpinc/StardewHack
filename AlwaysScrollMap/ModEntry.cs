66041eddf2eba980b8951a8bbfa20fd6062b2c8d
Bauke Conijn <bcmpinc@users.sourceforge.net>
2018-05-01 23:15:59 +0200

    Added actual mod implementation

--- AlwaysScrollMap/ModEntry.cs
+++ AlwaysScrollMap/ModEntry.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using StardewModdingAPI;
+using Harmony;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 
 namespace AlwaysScrollMap
 {
     public class ModEntry : Mod
     {
+        public static IMonitor Log;
         public override void Entry(IModHelper helper)
-        {}
+        {
+            Log = this.Monitor;
+            var harmony = HarmonyInstance.Create("bcmpinc.AlwaysScrollMap");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
 
+    [HarmonyPatch(typeof(StardewValley.Game1))]
+    [HarmonyPatch("UpdateViewPort")]
+    public static class Game1_UpdateViewPort
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            int start = codes.FindIndex(i => i.opcode == OpCodes.Ldsfld && AccessTools.Field(typeof(StardewValley.Game1), "viewportFreeze") == (FieldInfo)i.operand);
+            int end = codes.FindIndex(i => i.opcode == OpCodes.Ldfld && AccessTools.Field(typeof(StardewValley.GameLocation), "forceViewportPlayerFollow") == (FieldInfo)i.operand);
+            ModEntry.Log.Log($"start={start} end={end}");
+            codes.RemoveRange(start, end-start+2);
+            return codes;
+        }
     }
 }
 
