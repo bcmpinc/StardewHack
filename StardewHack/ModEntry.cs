ab5d021ced4b6e06ab93bbefedebe655a8e8738e
Bauke Conijn <bcmpinc@users.sourceforge.net>
2019-11-20 02:21:51 +0100

    Make the 'mods didn't load' warning work on android.

--- StardewHack/ModEntry.cs
+++ StardewHack/ModEntry.cs
@@ -1,14 +1,15 @@
 ﻿using StardewModdingAPI;
 using Harmony;
+using System.Collections.Generic;
 
 namespace StardewHack.Library
 {
-    public class ModEntry : StardewModdingAPI.Mod
+    public class ModEntry : Mod
     {
         /// <summary>
         /// During startup mods that are broken are added to this list. Used to produce an error message during startup.
         /// </summary>
-        static public System.Collections.Generic.List<string> broken_mods = new System.Collections.Generic.List<string>();
+        static public List<string> broken_mods = new List<string>();
     
         public override void Entry(IModHelper helper) {
             // Check versions
@@ -29,7 +30,7 @@ namespace StardewHack.Library
             CheckIncompatible(helper, "bcmpinc.TreeSpread",        new SemanticVersion(1,0,0));
             CheckIncompatible(helper, "bcmpinc.WearMoreRings",     new SemanticVersion(1,4,0));
             
-            Helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
+            Helper.Events.Display.MenuChanged += Display_MenuChanged;
         }
         
         public void CheckIncompatible(IModHelper helper, string uniqueID, SemanticVersion version) {
@@ -39,21 +40,32 @@ namespace StardewHack.Library
             }
         }
 
-        void GameLoop_GameLaunched(object sender, StardewModdingAPI.Events.GameLaunchedEventArgs e) {
+        void Display_MenuChanged(object sender, StardewModdingAPI.Events.MenuChangedEventArgs e)
+        {
+            // Fire the first time a menu is being loaded, but only fire once.
+            Helper.Events.Display.MenuChanged -= Display_MenuChanged;
+            
             // Create a warning message if patches failed to apply cleanly.
             if (broken_mods.Count==0) return;
             
-            System.Collections.Generic.List<string> mod_list = new System.Collections.Generic.List<string>();
+            var mod_list = new List<string>();
             foreach (var i in broken_mods) {
                 var mod = Helper.ModRegistry.Get(i).Manifest;
                 mod_list.Add($"{mod.Name} (v{mod.Version})");
             }
 
-            StardewValley.Game1.drawDialogueNoTyping(
+            // The message is a list containing a single string.
+            var dialogue = new List<string>() {
                 "StardewHack failed to apply some bytecode patches. The following mods won't work correctly or at all: " +
                 mod_list.Join() +
                 ". Check your console for further instructions."
-            );
+            };
+            
+            // Create the dialogue box. We can't pass a string directly as the signature differs between the PC and android version.
+            var box = new StardewValley.Menus.DialogueBox(dialogue);
+            StardewValley.Game1.activeClickableMenu = box;
+            StardewValley.Game1.dialogueUp = true;
+            box.finishTyping();
         }
     }
 }
