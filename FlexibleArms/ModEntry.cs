9142a39117f7b722d65ef4597923e1aa1922d840
Bauke Conijn <bcmpinc@users.sourceforge.net>
2024-03-24 00:04:18 +0100

    Added Flexible Arms mod.

---
+++ FlexibleArms/ModEntry.cs
@@ -0,0 +1,22 @@
+﻿using Entoarox.Framework;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewValley;
+using System;
+
+namespace StardewHack.FlexibleArms
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper) {
+            helper.Events.GameLoop.DayStarted += GameLoop_DayStarted;
+            plate = helper.Content.Load<Texture2D>("switch.png");
+        }
+
+        void GameLoop_DayStarted(object sender, StardewModdingAPI.Events.DayStartedEventArgs e) {
+            Game1.player.addItemToInventory(new PressurePlate());
+        }
+    }
+}
+
