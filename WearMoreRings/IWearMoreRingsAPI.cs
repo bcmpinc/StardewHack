a862d48e157d522f39fed2a732c038bd4db7f9c0
Bauke Conijn <bcmpinc@users.sourceforge.net>
2024-03-23 22:39:02 +0100

    Put info about the old API in a place where dev's probably will look for info.

--- WearMoreRings/IWearMoreRingsAPI.cs
+++ WearMoreRings/IWearMoreRingsAPI.cs
@@ -2,6 +2,46 @@
 
 namespace StardewHack.WearMoreRings
 {
+    /*
+     * If you're looking for the old API methods, those were implemented as follows.
+     * You can copy the code and use that instead of the old API.
+     * These work regardless of whether Wear More Rings is installed. 
+     * If Wear More rings is installed, GetAllRings will include the ring slots that are added by that mod.
+     * For combined rings, GetAllRings will return the rings used to create that combined ring, rather than the combined ring itself.
+     * If you need access to the combined rings, or want to manipulate the modded ring slots directly, that's only possible with the new API.
+     * 
+     * CountEquippedRings's which argument probably needs to be changed into a string for SDV 1.6.
+     */
+#if false
+    public int CountEquippedRings(Farmer f, int which) {
+        int res = 0;
+        foreach (var r in GetAllRings(f)) {
+            if (r.GetsEffectOfRing(which)) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    public IEnumerable<Ring> GetAllRings(Farmer f) {
+        if (f == null) throw new ArgumentNullException(nameof(f));
+        var stack = new Stack<Ring>();
+        stack.Push(f.leftRing.Value);
+        stack.Push(f.rightRing.Value);
+        while (stack.Count > 0) {
+            var ring = stack.Pop();
+            if (ring is CombinedRing) {
+                foreach (var cr in ((CombinedRing)ring).combinedRings) {
+                    stack.Push(cr);
+                }
+            } else if (ring != null) {
+                yield return ring;
+            }
+        }
+    }
+#endif
+
+
     public interface IWearMoreRingsAPI_2 {
         /// <summary>
         /// Get the mod's config setting for how many rings can be equipped.
